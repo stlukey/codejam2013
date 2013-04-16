210c954b0195e2c9135c4a5958109ad0aff1cd36
Luke Southam <luke@devthe.com>
2013-04-16 13:04:23 +0100

    fixed to original before multiproccessing attempt

--- qualifiers/c.py
+++ qualifiers/c.py
@@ -3,23 +3,19 @@
 # Luke Southam <luke@devthe.com>
 # GCJ Problem C. Fair and Square
 #
-
-from math import sqrt, ceil
-from multiprocessing import Pool
-
 square = lambda n: n**2
-squares = lambda min_, max_: map(square, range(int(ceil(sqrt(min_))), int(sqrt(max_)) + 1))
+squares = lambda min_, max_: map(square, filter(is_palindrome, range(int(ceil(sqrt(min_))), int(sqrt(max_)) + 1)))
 is_palindrome = lambda n: str(n) == str(n)[::-1]
-solve = lambda min_, max_: len(filter(is_palindrome, squares(min_, max_)))
 
 T = int(raw_input())
 
-inputs = []
-
-pool = Pool(processes=4)
-
 for test in xrange(T):
-	print "Case #%i: %s" % (test + 1, '\n'.join(map(str, squares(*map(int, raw_input().split())))))
+	min_, max_ = map(int, raw_input().split())
+
+	results = filter(is_palindrome, squares(min_, max_))
+
+	print "Case #%i: %i" % (test + 1, len(results))
+
 
 # P.S
 # Very easy solution in python, but not for large inputs.
